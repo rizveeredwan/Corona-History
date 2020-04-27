491521f5f8036808905698d0523e7ea871e8c19c
rizveeerprojects <rarndc@gmail.com>
2020-04-27 17:32:19 +0600

    april 27

--- bangladesh-data/code.py
+++ bangladesh-data/code.py
@@ -3,4 +3,4 @@ import tabula
 # Read pdf into list of DataFrame
 #df = tabula.read_pdf("Pdfs/Confirmed COVID-19 cases_upto_21_April 2020_last.pdf", pages='3')
 
-tabula.convert_into("Pdfs/District_dist_25_april.pdf", "output.csv", output_format="csv", pages='all')
+tabula.convert_into("Pdfs/Case_dist_27_april_update.pdf", "output.csv", output_format="csv", pages='all')
