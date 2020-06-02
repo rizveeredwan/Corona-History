af79fb8ad2845976668eaaf22b85c4260d9b3897
rizveeerprojects <rarndc@gmail.com>
2020-06-02 20:17:53 +0600

    may 31

--- bangladesh-data/DhakaAutomatedCoronaInfo.py
+++ bangladesh-data/DhakaAutomatedCoronaInfo.py
@@ -3,13 +3,20 @@ r = requests.get('http://covid19tracker.gov.bd/api/dhaka')
 obj = r.json()
 dhaka_dict={}
 list=[]
+
+key_map={
+    'Sher-E-Bangla':'Sher-E-Bangla Nagar'
+}
+
 for i in obj:
     print(i,i['name'])
-    dhaka_dict[i['name']]={}
-    dhaka_dict[i['name']]['bangla']=i['bnName']
-    dhaka_dict[i['name']]['confirmed']=int(i['confirmed'])
-    dhaka_dict[i['name']]['old-confirmed']=[]
-    dhaka_dict[i['name']]['flag']=0
+    if('Sher-E-Bangla' in i['name'] ):
+        i['name']=key_map['Sher-E-Bangla']
+    dhaka_dict[i['name'].strip()]={}
+    dhaka_dict[i['name'].strip()]['bangla']=i['bnName']
+    dhaka_dict[i['name'].strip()]['confirmed']=int(i['confirmed'])
+    dhaka_dict[i['name'].strip()]['old-confirmed']=[]
+    dhaka_dict[i['name'].strip()]['flag']=0
     list.append(i['name'])
 
 dates=[]
@@ -24,33 +31,47 @@ with open('DhakaInfo.csv','r') as file:
             dates.append(l[i])
         for i in range(1,len(lines)):
             l = lines[i].strip().split(',')
-            area = l[0]
+            area = l[0].strip()
             current_value = int(l[len(l)-1])
             if(area in list):
                 if(dhaka_dict[area]['confirmed']>current_value):
                     change=True
                     dhaka_dict[area]['flag']=1
-                    for j in range(2,len(l)):
+                    for j in range(1,len(l)):
                         dhaka_dict[area]['old-confirmed'].append(l[j])
             else:
                 print(area ," not found")
+                dhaka_dict[area]={}
+                dhaka_dict[area]['confirmed']=0
+                dhaka_dict[area]['old-confirmed']=[]
+                dhaka_dict[area]['flag']=1
+                for j in range(1,len(l)):
+                    dhaka_dict[area]['old-confirmed'].append(l[j])
+                list.append(area)
+
+for i in range(0,len(list)):
+    if(dhaka_dict[list[i]]['flag']==0):
+        change=True
+        break
 
 if(change==True):
     d=input('Give date: ')
     dates.append(d)
+    list = sorted(list)
     with open('DhakaInfo.csv','w') as file:
         string = 'Location'
         for i in range(0,len(dates)):
             string = string + ',' + dates[i]
         file.write(string+'\n')
-        for key in dhaka_dict:
+        for j in range(0,len(list)):
+            key=list[j]
             string = key
             if(len(dhaka_dict[key]['old-confirmed'])>0):
                 for i in range(0,len(dhaka_dict[key]['old-confirmed'])):
                     string = string + ',' + dhaka_dict[key]['old-confirmed'][i]
             else:
-                for i in range(0,len(dates)):
-                    string + string + ',' + '0'
+                for i in range(0,len(dates)-1):
+                    string = string + ',' + '0'
             string = string + ',' + str(dhaka_dict[key]['confirmed'])
             print(string)
             file.write(string+'\n')
